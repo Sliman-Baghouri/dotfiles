d0edfed44dbaddcbed53ef2675419f2c4a911768
Sliman <slimanbusiness@gmail.com>
2024-03-28 11:11:50 +0100

    removed unnecessary files

--- d.lua
+++
@@ -1,9 +0,0 @@
-local t = {}  -- Create an empty table
-
-t["hey"] = "this is value"  -- Insert a named value into the table
-
--- Printing the table
-for key, value in pairs(t) do
-    print(key, value)
-end
-
