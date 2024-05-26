7d59920ade8544010e6a6871d524736e9d7d6b69
Sliman Baghouri <slimanbusiness@gmail.com>
2024-05-26 03:37:50 +0100

    Fix: resolved ts server and deno lsp conflict

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,8 +1,8 @@
 return {
 	{
 		"williamboman/mason.nvim",
-  	lazy = false,
-  config = function()
+		lazy = false,
+		config = function()
 			require("mason").setup()
 		end,
 	},
@@ -19,41 +19,78 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfigKeymaps = require("keymaps").lsp_config
+			local lspconfigKeymaps = require("keymaps").lsp_config
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-        lspconfig.denols.setup {
-          on_attach = on_attach,
-          root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-        }
+			lspconfig.denols.setup({
+				root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+				init_options = {
+					lint = true,
+					unstable = true,
+					suggest = {
+						imports = {
+							hosts = {
+								["https://deno.land"] = true,
+								["https://cdn.nest.land"] = true,
+								["https://crux.land"] = true,
+							},
+						},
+					},
+				},
 
-        lspconfig.tsserver.setup {
-          on_attach = on_attach,
-          root_dir = lspconfig.util.root_pattern("package.json"),
-          single_file_support = false
-        }
-      -- lspconfig.denols.setup {
-      --   on_attach = function(client)
-      --     client.resolved_capabilities.document_formatting = false
-      --     client.resolved_capabilities.document_range_formatting = false
-      --     set_lsp_config(client)
-      --   end,
-      --   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-      -- }
+				on_attach = on_attach,
+			})
 
+			lspconfig.tsserver.setup({
+				on_attach = function(client, bufnr)
+					on_attach(client, bufnr)
+					vim.keymap.set("n", "<leader>ro", function()
+						vim.lsp.buf.execute_command({
+							command = "_typescript.organizeImports",
+							arguments = { vim.fn.expand("%:p") },
+						})
+					end, { buffer = bufnr, remap = false })
+				end,
+				root_dir = function(filename, bufnr)
+					local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.json")(filename)
+					if denoRootDir then
+						-- print('this seems to be a deno project; returning nil so that tsserver does not attach');
+						return nil
+						-- else
+						-- print('this seems to be a ts project; return root dir based on package.json')
+					end
 
-      -- lspconfig.tsserver.setup {
-      --   on_attach = function(client)
-      --     if client.config.flags then
-      --       client.config.flags.allow_incremental_sync = true
-      --     end
-      --     client.resolved_capabilities.document_formatting = false
-      --     set_lsp_config(client)
-      --   end,
-      --   root_dir = lspconfig.util.root_pattern("package.json"),
-      --   single_file_support = false
-      -- }
+					return lspconfig.util.root_pattern("package.json")(filename)
+				end,
+				single_file_support = false,
+			})
+			-- lspconfig.tsserver.setup {
+			--   on_attach = on_attach,
+			--   root_dir = lspconfig.util.root_pattern("package.json"),
+			--   single_file_support = false
+			-- }
+
+			-- lspconfig.denols.setup {
+			--   on_attach = function(client)
+			--     client.resolved_capabilities.document_formatting = false
+			--     client.resolved_capabilities.document_range_formatting = false
+			--     set_lsp_config(client)
+			--   end,
+			--   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+			-- }
+
+			-- lspconfig.tsserver.setup {
+			--   on_attach = function(client)
+			--     if client.config.flags then
+			--       client.config.flags.allow_incremental_sync = true
+			--     end
+			--     client.resolved_capabilities.document_formatting = false
+			--     set_lsp_config(client)
+			--   end,
+			--   root_dir = lspconfig.util.root_pattern("package.json"),
+			--   single_file_support = false
+			-- }
 
 			-- lspconfig.tsserver.setup({
 			-- 	on_attach = function(client)
@@ -63,7 +100,7 @@ return {
 			-- 		client.resolved_capabilities.document_formatting = false
 			-- 		set_lsp_config(client)
 			-- 	end,
-   --      })
+			--      })
 
 			-- lspconfig.tsserver.setup({
 			-- capabilities = capabilities,
@@ -75,11 +112,11 @@ return {
 				capabilities = capabilities,
 			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
+			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
-          lspconfigKeymaps(ev)
-        end,
+					lspconfigKeymaps(ev)
+				end,
 			})
 
 			vim.diagnostic.config({
@@ -94,7 +131,7 @@ return {
 				vim.fn.expand("$HOME/tools/flutter/"),
 			}
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
